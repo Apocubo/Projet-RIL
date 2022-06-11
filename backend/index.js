627e2619395922b45c2cfb0c9b3f2796413f2c26
Nicolas D <apo.detruio@gmail.com>
2022-06-11 15:04:33 +0200

    change of issues

--- backend/index.js
+++ backend/index.js
@@ -10,4 +10,20 @@ const connection = mysql.createPool({
   database: process.env.MYSQL_DATABASE || "test",
 });
 
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM Student", (err, rows) => {
+    if (err) {
+      res.json({
+        success: false,
+        err,
+      });
+    } else {
+      res.json({
+        success: true,
+        rows,
+      });
+    }
+  });
+});
+
 app.listen(5000, () => console.log('listining on port 5000'));
