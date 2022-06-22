84562af142a3d63c14d672c0b77d2b463184da98
Nabil <nabilnaitaddi1@gmail.com>
2022-06-22 14:57:42 +0200

    Développement page connexion, inscription + ajout des routers vers ces pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,21 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { InscriptionComponent } from './inscription/inscription.component'
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
+  
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  
+  {
+    path: 'Connexion', component : ConnexionComponent
+  },
 
   {
-    path: 'inscription', component: InscriptionComponent
+    path: 'Inscription', component: InscriptionComponent
   },
+
 ];
 
 @NgModule({
